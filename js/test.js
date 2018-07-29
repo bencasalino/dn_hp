8fa03c74895a9939c6a36131d8ecbcbf416b782d
Ben Casalino <bencasalino@gmail.com>
2018-07-29 08:41:25 -0600

    sunday

---
+++ js/test.js
@@ -0,0 +1,68 @@
+var data =
+  '{"name": "mkyong","age": 30,"address": {"streetAddress": "88 8nd Street","city": "New York"},"phoneNumber": [{"type": "home","number": "111 111-1111"},{"type": "fax","number": "222 222-2222"}]}';
+
+var json = JSON.parse(data);
+
+json["name"]; //mkyong
+json.name; //mkyong
+
+//  functions
+
+filterSelection("all");
+function filterSelection(c) {
+  var x, i;
+  x = document.getElementsByClassName("filterDiv");
+  if (c == "all") c = "";
+  for (i = 0; i < x.length; i++) {
+    w3RemoveClass(x[i], "show");
+    if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+  }
+}
+
+function w3AddClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    if (arr1.indexOf(arr2[i]) == -1) {
+      element.className += " " + arr2[i];
+    }
+  }
+}
+
+function w3RemoveClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    while (arr1.indexOf(arr2[i]) > -1) {
+      arr1.splice(arr1.indexOf(arr2[i]), 1);
+    }
+  }
+  element.className = arr1.join(" ");
+}
+
+// Add active class to the current button (highlight it)
+var btnContainer = document.getElementById("myBtnContainer");
+var btns = btnContainer.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
+
+// function petTemplate(pet) {
+//   return `
+//     <div>
+//     <p class="event-icon">${pet.type}</p>
+//     <p class="event-date">${pet.year}</p>
+//       <h2 class="event-text">${pet.eventItem}</h2>
+//     </div>
+//   `;
+// }
+
+// document.getElementsById("#season-2018-off").innerHTML = `
+//   ${petsData.map(petTemplate).join("")}
+// `;
